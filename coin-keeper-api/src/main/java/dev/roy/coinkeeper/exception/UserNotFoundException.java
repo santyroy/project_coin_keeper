6ae4429dd1d9a09771dd0b17ddf2cb6913f6b52e
Sougata Roy <sougata.roy26@yahoo.com>
2024-03-16 05:00:41 +0530

    Added UserNotFoundException

---
+++ coin-keeper-api/src/main/java/dev/roy/coinkeeper/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package dev.roy.coinkeeper.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
