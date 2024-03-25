ffd14dbb2bda74c142c183ec93d195d516ae740e
Sougata Roy <sougata.roy26@yahoo.com>
2024-03-25 22:18:46 +0530

    Fix Invalid refresh token exception

--- coin-keeper-api/src/main/java/dev/roy/coinkeeper/exception/InvalidRefreshTokenException.java
+++ coin-keeper-api/src/main/java/dev/roy/coinkeeper/exception/InvalidRefreshTokenException.java
@@ -2,5 +2,6 @@ package dev.roy.coinkeeper.exception;
 
 public class InvalidRefreshTokenException extends RuntimeException {
     public InvalidRefreshTokenException(String message) {
+        super(message);
     }
 }
